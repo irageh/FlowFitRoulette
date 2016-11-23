a4f179947948b000dcd7ba7dadf43337f581afb6
Ibs Rageh <irageh@gmail.com>
2016-11-23 15:42:03 -0800

    Basic grid with intro anims

---
+++ Assets/Scripts/UI/ExerciseGridDisplay.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+
+public class ExerciseGridDisplay : MonoBehaviour 
+{
+	private int _index;
+
+	public enum DisplayState
+	{
+		Hidden,
+		Revealing,
+		Revealed,
+		Active
+	}
+	private DisplayState _displayState = DisplayState.Hidden;
+
+	void Start () 
+	{
+	
+	}
+
+	public void Initialize(int index, float introAnimDelay)
+	{
+		_index = index;
+
+		_displayState = DisplayState.Hidden;
+		gameObject.transform.DORotate(new Vector3(720.0f, 0.0f, 0.0f), 2.25f).SetRelative(true).SetEase(Ease.OutBounce).SetDelay(introAnimDelay).OnStart(OnRevealAnimStart).OnComplete(OnRevealAnimComplete);
+	}
+
+	private void OnRevealAnimStart()
+	{
+		_displayState = DisplayState.Revealing;
+	}
+
+	private void OnRevealAnimComplete()
+	{
+		_displayState = DisplayState.Revealed;
+	}
+
+	public void RevealExercise()
+	{
+
+	}
+
+	public void OnClick()
+	{
+		Debug.Log ("Clicked on exercise: " + _index + ": " + gameObject.name);
+	}
+}
---
+++ Assets/Scripts/UI/ExerciseGridView.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ExerciseGridView : MonoBehaviour 
+{
+	public List<ExerciseGridDisplay> exerciseGrid;
+
+	void Start () 
+	{
+		float delayPerEntry = 0.15f;
+		float delay = 0.0f;
+		for (int i = 0; i < exerciseGrid.Count; ++i) 
+		{
+			exerciseGrid[i].Initialize(i, delay);
+
+			delay += delayPerEntry;
+		}
+	}
+}
